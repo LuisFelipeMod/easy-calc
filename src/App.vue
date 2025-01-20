2018cb9a78d1c074ce1384d3ab968de502b9f815
LuisFelipeMod <luisfelipetecno@gmail.com>
2025-01-20 11:52:12 -0300

    init: initial structure

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div>
+    <Header msg="Easy Calc" />
+    <main>
+
+    </main>
+
+    <footer>
+      <code>
+        Desenvolvido por: <a href="https://github.com/LuisFelipeMod" target="_blank">Luis Modesto</a>
+      </code>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+
+  footer{
+    display: flex;
+    justify-content: center;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <header class="header">
+    <a href="/" target="_self" class="logo">
+      <img src="../assets/easy-calc.svg" class="logo-img" width="150" height="59" alt="icone da easy calc">
+    </a>
+  </header>
+</template>
+
+<style scoped>
+header {
+  position: sticky;
+  background: #E85B4A;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
+}
+
+.logo{
+  margin-left: 1rem;
+}
+
+nav{
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  color: #fff;
+  padding: 0.8rem;
+  border-radius: 1rem;
+  line-height: normal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+}
+</style>
